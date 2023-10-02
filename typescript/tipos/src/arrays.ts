4a2fc1c3c66844a83d78caf11e9b9320ee76f8e3
Lívia Nascimento <liviapnd@gmail.com>
2023-10-02 13:41:54 -0300

    feat: declaração de arrays comuns e multitype

---
+++ typescript/tipos/src/arrays.ts
@@ -0,0 +1,6 @@
+// array comum
+let alunos: string[] = ["Maria", "Joana", "Fernando"];
+let professores: Array<string> = ["Clara", "Márcia", "Mariana"];
+
+// array multi types
+let AlunoOuMatricula: (string | number)[] = ["Rafaela", "Sara", 12645487, "Tatiana"];
