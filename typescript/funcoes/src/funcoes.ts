015a8c3627476b45512cbf317c684d0003286e79
Lívia Nascimento <liviapnd@gmail.com>
2023-10-03 13:02:57 -0300

    feat: função soma

---
+++ typescript/funcoes/src/funcoes.ts
@@ -0,0 +1,11 @@
+function soma(x: number, y: number): number {
+    return x + y;
+}
+
+const resultadoSoma: number = soma(3, 4);
+
+const somaArrow = (x: number, y: number): number => {
+    return x + y;
+}
+
+const resultadoSomaArrow: number = soma(3, 4);
